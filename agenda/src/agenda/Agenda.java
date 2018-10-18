b11bf59c3e8a7c2f6fcff645b6e374e2960d5742
Kayke Bonafé <kayke.bonafe98@gmail.com>
2018-10-17 23:55:07 -0300

    Projeto finalizado e com Javadoc

--- agenda/src/agenda/Agenda.java
+++ agenda/src/agenda/Agenda.java
@@ -3,29 +3,39 @@ package agenda;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**
+ *
+ * @author Kayke Bonafé de Luca
+ * R.A.: 22.217.003-7
+ * 
+ */
 public class Agenda {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-       
+        ArrayList <PhoneBook> agendaVector = new ArrayList<>();//Cria um ArrayList
         while (true) {
             Scanner entrada = new Scanner(System.in);//Cria um Scanner
-            ArrayList <PhoneBook> agendaVector = new ArrayList<>();//Cria um ArrayList
         
             System.out.println("Entre com uma das seguintes opções:\n");//Menu de opções
-            System.out.println("n [nova entrada]");
-            System.out.println("d [apaga registro da agenda]");
-            System.out.println("p [imprime toda a agenda]");
-            System.out.println("q [sai do programa]");
+            System.out.println("n - Novo Contato");
+            System.out.println("d - Apaga o contato escolhido");
+            System.out.println("p - Mostra todos os contatos adicionados na agenda");
+            System.out.println("q - Encerra o programa");
             System.out.println(" ");
             
             String in = entrada.next();//Scanner de entrada do índice do menu
-            String nome = entrada.next();//Scanner de input do nome
-            Integer telefone = entrada.nextInt();//Scanner de input de telefone
-            
+              
             switch (in){
                 
                 case ("n"):
-                    System.out.println("Entre com o nome e o telefone: \n");
+                    System.out.println("Digite o nome:");
+                    String nome = entrada.next();//Scanner de input do nome
+                    System.out.println("Digite o telefone:");
+                    Integer telefone = entrada.nextInt();//Scanner de input de telefone
                     agendaVector.add(new PhoneBook(nome, telefone));
                     break;
             
@@ -36,9 +46,9 @@ public class Agenda {
                        final PhoneBook f = agendaVector.get(i);
                         System.out.println("\tChecando <" + agendaVector.get(i).nome + ">");
                         if(f.nome.equals(nome)){
-                            System.out.println("Ele foi achado no índice: " + i);
+                            System.out.println("Ele foi encontrado no índice: " + i);
                             agendaVector.remove(i);
-                            System.out.println("Excluido com sucesso");
+                            System.out.println("Excluído com sucesso");
                         }
                     }
                     break;
--- agenda/src/agenda/PhoneBook.java
+++ agenda/src/agenda/PhoneBook.java
@@ -1,30 +1,60 @@
 package agenda;
 
+/**
+ *
+ * @author Kayke Bonafé de Luca
+ * R.A.: 22.217.003-7
+ * 
+ */
 public class PhoneBook {
 
-
- public PhoneBook(String nome, int telefone) {
+    /**
+     *
+     * Construtor da classe
+     * 
+     * @param nome
+     * @param telefone
+     */
+    public PhoneBook(String nome, int telefone) {
         this.nome = nome;
         this.telefone = telefone;
     }
 
+    /**
+     *
+     * @return o nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     *
+     * @param nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     *
+     * @return o telefone
+     */
     public int getTelefone() {
         return telefone;
     }
 
+    /**
+     *
+     * @param telefone
+     */
     public void setTelefone(int telefone) {
         this.telefone = telefone;
     }
 
-   
+    /**
+     * Variavéis da classe
+     */
     protected String nome;
     private int telefone;
 
